f952cb7bdbe0b41a6daab6ad49b8daf01071b3c4
shway250 <jackson_duhon@yahoo.com>
2016-12-10 12:00:14 -0800

    cleaned up sketch files. Now there is nothing but the working instance mode examples, and the working global mode snow objects

--- p5-sketches/snow.js
+++ p5-sketches/snow.js
@@ -1,124 +1,91 @@
-// var snow = [];
-// var number_of_flakes = 150;
+var snow = [];
+var number_of_flakes = 150;
 
-// function setup() {
-//   var canvas1 = createCanvas(300, 300);
-//   canvas1.parent("canvas1");
-//   // Create object
-//   for (var i = 0; i < number_of_flakes; i++){
-//     snow[i] = new Jitter();
-//   }
-// }
+function setup() {
+  var canvas1 = createCanvas(300, 300);
+  canvas1.parent("canvas1");
+  // Create object
+  for (var i = 0; i < number_of_flakes; i++){
+    snow[i] = new Jitter();
+  }
+}
 
-// function draw() {
-//   background(255);
-//   for (var i = 0; i < number_of_flakes; i++){
-//     snow[i].move();
-//     snow[i].display();
-//     snow[i].bringUp();
-//   }
-// }
+function draw() {
+  background(255);
+  for (var i = 0; i < number_of_flakes; i++){
+    snow[i].move();
+    snow[i].display();
+    snow[i].bringUp();
+  }
+}
 
-// // Jitter class
-// function Jitter() {
-//   this.x = random(width);
-//   this.y = random(height);
-//   this.speed = 1;
-//   //rgb values
-//   this.r = random(0,100);
-//   this.g = random(0,100);
-//   this.b = random(0,255);
+// Jitter class
+function Jitter() {
+  this.x = random(width);
+  this.y = random(height);
+  this.speed = 1;
+  //rgb values
+  this.r = random(0,100);
+  this.g = random(0,100);
+  this.b = random(0,255);
 
-//   this.move = function() {
-//     this.x += random(-this.speed, this.speed);
-//     this.y += random(-this.speed, this.speed* random(1, 10));
-//   };
+  this.move = function() {
+    this.x += random(-this.speed, this.speed);
+    this.y += random(-this.speed, this.speed* random(1, 10));
+  };
 
-//   this.display = function() {
-//     stroke(255);
-//     strokeWeight(0);
-//     //making Shape
-//     rect(this.x,this.y,3,30);
-//   }
+  this.display = function() {
+    stroke(255);
+    strokeWeight(0);
+    //making Shape
+    rect(this.x,this.y,3,30);
+  }
 
-//   this.bringUp = function() {
-//     fill(this.r,this.g,this.b);
-//     //X Axis
-//     if (this.x > width){
-//     this.x=0;
-//     }
-//     else if (this.x < 0){
-//     this.x=width;
-//     }
-//     //Y Axis
-//     if (this.y > height){
-//     this.y = 0;
-//     }
-//   }
-// };
-
-
-// function setup() {
-//   var canvas1 = createCanvas(100, 300);
-//   canvas1.parent("canvas1");
-  
-// }
-
-// function draw() {
-//   background(0);
-  
-// }
+  this.bringUp = function() {
+    fill(this.r,this.g,this.b);
+    //X Axis
+    if (this.x > width){
+    this.x=0;
+    }
+    else if (this.x < 0){
+    this.x=width;
+    }
+    //Y Axis
+    if (this.y > height){
+    this.y = 0;
+    }
+  }
+};
 
 
-// var t = function( p ) { 
-//   var x = 100.0; 
-//   var y = 100; 
-//   var speed = 2.5; 
+// var sketch1 = function(p) {
+//   p.x = 100;
+//   p.y = 100;
 //   p.setup = function() {
-//     p.createCanvas(400, 200);
-//   };
-
+//     var canvas1 = p.createCanvas(200, 200);
+//     canvas1.parent("canvas1");
+//     p.background(51);
+//   }
 //   p.draw = function() {
-//     p.background(100);
-//     p.fill(1);
-//     x += speed; 
-//     if(x > p.width){
-//       x = 0; 
-//     }
-//     p.ellipse(x,y,50,50);
+//     p.fill(255, 0, 200, 25);
+//     p.noStroke();
+//     p.ellipse(p.x, p.y, 48, 48);
 
-//   };
-// };
-// var myp51 = new p5(t, 'canvas1');
-
-var sketch1 = function(p) {
-  p.x = 100;
-  p.y = 100;
-  p.setup = function() {
-    var canvas1 = p.createCanvas(200, 200);
-    canvas1.parent("canvas1");
-    p.background(51);
-  }
-  p.draw = function() {
-    p.fill(255, 0, 200, 25);
-    p.noStroke();
-    p.ellipse(p.x, p.y, 48, 48);
-
-    p.x = p.x + p.random(-10, 10);
-    p.y = p.y + p.random(-10, 10);
-  }
-}
+//     p.x = p.x + p.random(-10, 10);
+//     p.y = p.y + p.random(-10, 10);
+//   }
+// }
 
 
-var myp5_1 = new p5(sketch1);
+// var myp5_1 = new p5(sketch1);
 
-function resetBackground() {
-  myp5_1.x = myp5_1.width/2;
-  myp5_1.y = myp5_1.height/2;
-  myp5_1.background(51);
-}
+// function resetBackground() {
+//   myp5_1.x = myp5_1.width/2;
+//   myp5_1.y = myp5_1.height/2;
+//   myp5_1.background(51);
+// }
 
-setInterval(resetBackground, 3000);
+// setInterval(resetBackground, 3000);
 
 
 
